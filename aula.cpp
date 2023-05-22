5e400202014232cf2d0f226d2039dc6204cd6070
Rafael Tsutomu Jyoboji <202219060230@lab02-l>
2023-05-22 14:26:24 -0400

    feat: adicionando funcionalidade

--- aula.cpp
+++ aula.cpp
@@ -5,5 +5,6 @@ int main(){
     int a = 100;
     int b = 200;
 
-    printf("A soma é igual a: %d", a+b);
+    printf("A soma é igual a: %d", a-b);
+    printf("A subtração é igual a: %d", a-b);
 }
